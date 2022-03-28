a70110fabc7382b0ce1b1953826cf5f7a76a9758
Md Mahmudul Hasan Reza <45889540+mhreza76@users.noreply.github.com>
2022-03-28 18:12:33 +0600

    Add files via upload

--- sql_practice.sql
+++ sql_practice.sql
@@ -11,3 +11,30 @@ BEGIN
     END WHILE;
     SELECT string_value;
 END
+
+
+
+---Problem Statement.
+		P(R) represents a pattern drawn by Julia in R rows. The following pattern represents P(5):
+
+		* 
+		* * 
+		* * * 
+		* * * * 
+		* * * * *
+		Write a query to print the pattern P(20).
+
+---Solution		
+set @n_row := 0;
+    select repeat('* ', @n_row := @n_row + 1)
+    from information_schema.tables
+    where @n_row < 20
+	
+// another solution
+set @n_row := 0;
+    select repeat('* ', @n_row := n_row + 1)
+    from information_schema.tables
+    limit 20
+	
+	
+	
